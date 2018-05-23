c8b9f4c7d7d2158ecb4d79b7d11f170bc1c3d27f
Marta <martaguilar.soto@gmail.com>
2018-05-23 11:10:10 +0200

    return only test trivial

--- __tests__/spec.js
+++ __tests__/spec.js
@@ -1,152 +1,152 @@
-// const pug = require('pug');
-// const saberganarGame = require('../src/main');
-// const saberganarQuestionsNavigator = require('../src/questionsNavigator');
+const pug = require('pug');
+const saberganarGame = require('../src/main');
+const saberganarQuestionsNavigator = require('../src/questionsNavigator');
 
-// describe("the questions navigator", () => {
-//     let questions = [
-//         {
-//             id: 10,
-//             title: 'Foo',
-//             answers: [
-//                 {id: 0, answer: '25'},
-//                 {id: 1, answer: '33'},
-//                 {id: 2, answer: '37'}
-//             ],
-//             correctAnswer: {id: 2}
-//         },
-//         {
-//             id: 11,
-//             title: 'Pero que dices muchacho?',
-//             answers: [
-//                 {id: 0, answer: 'Lusaka'},
-//                 {id: 1, answer: 'Harare'},
-//                 {id: 2, answer: 'Canarias'}
-//             ],
-//             correctAnswer: {id: 2}
-//         }
-//     ];
-//     let questionsNavigator;
+describe("the questions navigator", () => {
+    let questions = [
+        {
+            id: 10,
+            title: 'Foo',
+            answers: [
+                { id: 0, answer: '25' },
+                { id: 1, answer: '33' },
+                { id: 2, answer: '37' }
+            ],
+            correctAnswer: { id: 2 }
+        },
+        {
+            id: 11,
+            title: 'Pero que dices muchacho?',
+            answers: [
+                { id: 0, answer: 'Lusaka' },
+                { id: 1, answer: 'Harare' },
+                { id: 2, answer: 'Canarias' }
+            ],
+            correctAnswer: { id: 2 }
+        }
+    ];
+    let questionsNavigator;
 
-//     beforeEach(function () {
-//         questionsNavigator = saberganarQuestionsNavigator.questionsNavigator(questions);
-//     });
+    beforeEach(function () {
+        questionsNavigator = saberganarQuestionsNavigator.questionsNavigator(questions);
+    });
 
-//     it("gest the current question", () => {
-//         let question = questionsNavigator.getNextQuestion();
-//         expect(questions).toContain(question);
-//     });
+    it("gest the current question", () => {
+        let question = questionsNavigator.getNextQuestion();
+        expect(questions).toContain(question);
+    });
 
-//     it("is always pointing to a question", () => {
-//         let question = questionsNavigator.getNextQuestion();
-//         expect(questions).toContain(question);
-//     });
+    it("is always pointing to a question", () => {
+        let question = questionsNavigator.getNextQuestion();
+        expect(questions).toContain(question);
+    });
 
-//     it("does not repeat the last question", () => {
-//         let question1 = questionsNavigator.getNextQuestion();
-//         let question2 = questionsNavigator.getNextQuestion();
-//         let question3 = questionsNavigator.getNextQuestion();
+    it("does not repeat the last question", () => {
+        let question1 = questionsNavigator.getNextQuestion();
+        let question2 = questionsNavigator.getNextQuestion();
+        let question3 = questionsNavigator.getNextQuestion();
 
-//         expect(question1).not.toEqual(question2);
-//         expect(question2).not.toEqual(question3);
-//     });
+        expect(question1).not.toEqual(question2);
+        expect(question2).not.toEqual(question3);
+    });
 
-//     it("knows when the questions are all visited", () => {
-//         questionsNavigator.getNextQuestion();
-//         expect(questionsNavigator.areThereNonVisitedQuestions()).toBeTruthy();
-//         questionsNavigator.getNextQuestion();
-//         expect(questionsNavigator.areThereNonVisitedQuestions()).toBeFalsy();
-//     });
-// });
+    it("knows when the questions are all visited", () => {
+        questionsNavigator.getNextQuestion();
+        expect(questionsNavigator.areThereNonVisitedQuestions()).toBeTruthy();
+        questionsNavigator.getNextQuestion();
+        expect(questionsNavigator.areThereNonVisitedQuestions()).toBeFalsy();
+    });
+});
 
-// describe("the game", function () {
-//     var app;
-//     var questions = [
-//         {
-//             id: 10,
-//             title: 'Foo',
-//             answers: [
-//                 { id: 0, answer: '25' },
-//                 { id: 1, answer: '33' },
-//                 { id: 2, answer: '37' }
-//             ],
-//             correctAnswer: { id: 2 }
-//         },
-//         {
-//             id: 11,
-//             title: 'Pero que dices muchacho?',
-//             answers: [
-//                 { id: 0, answer: 'Lusaka' },
-//                 { id: 1, answer: 'Harare' },
-//                 { id: 2, answer: 'Canarias' }
-//             ],
-//             correctAnswer: { id: 2 }
-//         }
-//     ];
-//     beforeEach(function () {
-//         document.body.innerHTML = pug.compileFile('./views/main.pug', null)();
-//         app = saberganarGame.game(saberganarQuestionsNavigator.questionsNavigator);
-//         app.setServerData(questions);
-//         app.start();
-//     });
+describe("the game", function () {
+    var app;
+    var questions = [
+        {
+            id: 10,
+            title: 'Foo',
+            answers: [
+                { id: 0, answer: '25' },
+                { id: 1, answer: '33' },
+                { id: 2, answer: '37' }
+            ],
+            correctAnswer: { id: 2 }
+        },
+        {
+            id: 11,
+            title: 'Pero que dices muchacho?',
+            answers: [
+                { id: 0, answer: 'Lusaka' },
+                { id: 1, answer: 'Harare' },
+                { id: 2, answer: 'Canarias' }
+            ],
+            correctAnswer: { id: 2 }
+        }
+    ];
+    beforeEach(function () {
+        document.body.innerHTML = pug.compileFile('./views/main.pug', null)();
+        app = saberganarGame.game(saberganarQuestionsNavigator.questionsNavigator);
+        app.setServerData(questions);
+        app.start();
+    });
 
-//     it('loads the markup', function () {
-//         expect(
-//             document.getElementById('start--button'))
-//             .not.toBeNull();
-//     });
+    it('loads the markup', function () {
+        expect(
+            document.getElementById('start--button'))
+            .not.toBeNull();
+    });
 
-//     it('answers a question', function () {
-//         startGame();
-//         selectFirstAnswer();
+    it('answers a question', function () {
+        startGame();
+        selectFirstAnswer();
 
-//         goToNextQuestion();
+        goToNextQuestion();
 
-//         assertThatSecondQuestionIsRendered();
-//     });
+        assertThatSecondQuestionIsRendered();
+    });
 
-//     it("restart the counter time", function (done) {
-//         startGame();
-//         selectFirstAnswer();
-//         goToNextQuestion();
-//         const counterInDOM = document.querySelector(".clock");
+    it("restart the counter time", function (done) {
+        startGame();
+        selectFirstAnswer();
+        goToNextQuestion();
+        const counterInDOM = document.querySelector(".clock");
 
-//         function onTimeout() {
-//             expect(parseInt(counterInDOM.innerHTML)).toEqual(9);
-//             done();
-//         }
+        function onTimeout() {
+            expect(parseInt(counterInDOM.innerHTML)).toEqual(9);
+            done();
+        }
 
-//         setTimeout(onTimeout, 1000);
-//     });
+        setTimeout(onTimeout, 1000);
+    });
 
-//     function getQuestionTitleElement() {
-//         let questionTitle = document.querySelector('.question--title');
-//         return questionTitle;
-//     }
+    function getQuestionTitleElement() {
+        let questionTitle = document.querySelector('.question--title');
+        return questionTitle;
+    }
 
-//     function expectFirstQuestionToBeRendered() {
-//         let questionTitle = getQuestionTitleElement();
-//         expect(Number(questionTitle.id)).toEqual(Number(questions[0].id));
-//     }
+    function expectFirstQuestionToBeRendered() {
+        let questionTitle = getQuestionTitleElement();
+        expect(Number(questionTitle.id)).toEqual(Number(questions[0].id));
+    }
 
-//     function startGame() {
-//         let buttonStart = document.getElementById('start--button');
-//         buttonStart.click();
-//         expectFirstQuestionToBeRendered();
-//     }
+    function startGame() {
+        let buttonStart = document.getElementById('start--button');
+        buttonStart.click();
+        expectFirstQuestionToBeRendered();
+    }
 
-//     function selectFirstAnswer() {
-//         let firstAnswer = document.getElementsByTagName('input')[0];
-//         firstAnswer.click();
-//     }
+    function selectFirstAnswer() {
+        let firstAnswer = document.getElementsByTagName('input')[0];
+        firstAnswer.click();
+    }
 
-//     function goToNextQuestion() {
-//         let nextQuestionButton = document.getElementById('next--question--button');
-//         nextQuestionButton.click();
-//     }
+    function goToNextQuestion() {
+        let nextQuestionButton = document.getElementById('next--question--button');
+        nextQuestionButton.click();
+    }
 
-//     function assertThatSecondQuestionIsRendered() {
-//         let questionTitle = getQuestionTitleElement();
-//         expect(Number(questionTitle.id)).toEqual(Number(questions[1].id));
-//         expect(questionTitle.innerHTML).toEqual(questions[1].title);
-//     }
-// });
+    function assertThatSecondQuestionIsRendered() {
+        let questionTitle = getQuestionTitleElement();
+        expect(Number(questionTitle.id)).toEqual(Number(questions[1].id));
+        expect(questionTitle.innerHTML).toEqual(questions[1].title);
+    }
+});
--- src/questionsNavigator.js
+++ src/questionsNavigator.js
@@ -1,39 +1,39 @@
-// var saberganar = saberganar || {};
+var saberganar = saberganar || {};
 
-// saberganar.questionsNavigator = function (questions) {
-//     let questionsIndex = 0;
-//     let nonVisitedQuestions = true;
+saberganar.questionsNavigator = function (questions) {
+    let questionsIndex = 0;
+    let nonVisitedQuestions = true;
 
-//     function areThereNonVisitedQuestions() {
-//         return nonVisitedQuestions;
-//     }
+    function areThereNonVisitedQuestions() {
+        return nonVisitedQuestions;
+    }
 
-//     function resetQuestions() {
-//         questionsIndex = 0;
-//     }
+    function resetQuestions() {
+        questionsIndex = 0;
+    }
 
-//     function goToNextQuestion() {
-//         questionsIndex++;
-//     }
+    function goToNextQuestion() {
+        questionsIndex++;
+    }
 
-//     function getNextQuestion() {
-//         let question = questions[questionsIndex];
-//         goToNextQuestion();
-//         if (questionsIndex >= questions.length) {
-//             nonVisitedQuestions = false;
-//             resetQuestions();
-//         }
-//         return question;
-//     }
+    function getNextQuestion() {
+        let question = questions[questionsIndex];
+        goToNextQuestion();
+        if (questionsIndex >= questions.length) {
+            nonVisitedQuestions = false;
+            resetQuestions();
+        }
+        return question;
+    }
 
-//     return {
-//         areThereNonVisitedQuestions,
-//         getNextQuestion: getNextQuestion
-//     };
-// };
+    return {
+        areThereNonVisitedQuestions,
+        getNextQuestion: getNextQuestion
+    };
+};
 
-// // be able to import the file in node
-// if (typeof(module) != 'undefined'){
-//     module.exports = saberganar;
-// }
+// be able to import the file in node
+if (typeof (module) != 'undefined') {
+    module.exports = saberganar;
+}
 
